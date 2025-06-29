edd3633a16e9024219720e14db7f5d683af5b4c7
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-06-29 12:53:17 +0530

    Add Problem326.java with implementation of isPowerOfThree solution to check if a number is a power of three

---
+++ Probem326.java
@@ -0,0 +1,14 @@
+public class Probem326 {
+    public static void main(String[] args) {
+        // Example usage
+        int n = 27;
+        Solution solution = new Solution();
+        System.out.println(solution.isPowerOfThree(n)); // Output: true
+    }
+}
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+}
