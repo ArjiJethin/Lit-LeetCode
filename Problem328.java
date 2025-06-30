1650167ab07adf2567e637f39d6256b1ee521a78
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-06-30 20:23:02 +0530

    Add Problem328.java with implementation of oddEvenList to rearrange nodes in odd-even order in a linked list

---
+++ Problem328.java
@@ -0,0 +1,30 @@
+public class Problem328 {
+    public static void main(String[] args) {
+        // Example usage
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        Solution solution = new Solution();
+        ListNode result = solution.oddEvenList(head);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode a = head;
+        ListNode b = head.next, c = b;
+        while (b != null && b.next != null) {
+            a.next = b.next;
+            a = a.next;
+            b.next = a.next;
+            b = b.next;
+        }
+        a.next = c;
+        return head;
+    }
+}
