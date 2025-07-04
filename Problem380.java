9553748b98762cb64099f96fa0d029215bcf4c6b
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-07-04 17:33:03 +0530

    Add Problem380.java with implementation of RandomizedSet to allow insertion, removal, and retrieval of random elements

---
+++ Problem380.java
@@ -0,0 +1,52 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Problem380 {
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.insert(1)); // true
+        System.out.println(randomizedSet.remove(2)); // false
+        System.out.println(randomizedSet.insert(2)); // true
+        System.out.println(randomizedSet.getRandom()); // 1 or 2
+        System.out.println(randomizedSet.remove(1)); // true
+        System.out.println(randomizedSet.insert(2)); // false
+        System.out.println(randomizedSet.getRandom()); // 2
+    }
+}
+
+class RandomizedSet {
+    private Map<Integer, Integer> d = new HashMap<>();
+    private List<Integer> q = new ArrayList<>();
+    private Random rnd = new Random();
+
+    public RandomizedSet() {
+    }
+
+    public boolean insert(int val) {
+        if (d.containsKey(val)) {
+            return false;
+        }
+        d.put(val, q.size());
+        q.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!d.containsKey(val)) {
+            return false;
+        }
+        int i = d.get(val);
+        d.put(q.get(q.size() - 1), i);
+        q.set(i, q.get(q.size() - 1));
+        q.remove(q.size() - 1);
+        d.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return q.get(rnd.nextInt(q.size()));
+    }
+}
