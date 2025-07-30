a22d2639a7fbd09495c30c73438f5b83f8a1eef4
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-07-30 12:14:29 +0530

    Add implementation for Problem437: Path Sum III

---
+++ Problem437.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem437 {
+    public static void main(String[] args) {
+        // Example usage
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(-3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(2);
+        root.right.right = new TreeNode(11);
+        root.left.left.left = new TreeNode(3);
+        root.left.left.right = new TreeNode(-2);
+        root.left.right.right = new TreeNode(1);
+
+        Solution solution = new Solution();
+        int targetSum = 8;
+        int result = solution.pathSum(root, targetSum);
+        System.out.println("Number of paths with sum " + targetSum + ": " + result); // Expected output: 3
+    }
+}
+
+class Solution {
+    private Map<Long, Integer> cnt = new HashMap<>();
+    private int targetSum;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        cnt.put(0L, 1);
+        this.targetSum = targetSum;
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, long s) {
+        if (node == null) {
+            return 0;
+        }
+        s += node.val;
+        int ans = cnt.getOrDefault(s - targetSum, 0);
+        cnt.merge(s, 1, Integer::sum);
+        ans += dfs(node.left, s);
+        ans += dfs(node.right, s);
+        cnt.merge(s, -1, Integer::sum);
+        return ans;
+    }
+}
