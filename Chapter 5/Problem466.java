c4dfa3499f7776b9f7c9de83fe77e442667360b5
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-09-08 19:59:52 +0530

    Add implementation for Problem466: Maximum Repetitions of Substring

---
+++ Chapter 5/Problem466.java
@@ -0,0 +1,38 @@
+public class Problem466 {
+    public static void main(String[] args) {
+        // Example usage
+        String s1 = "acb";
+        int n1 = 4;
+        String s2 = "ab";
+        int n2 = 2;
+        Solution solution = new Solution();
+        int result = solution.getMaxRepetitions(s1, n1, s2, n2);
+        System.out.println("Maximum repetitions: " + result); // Expected output: 2
+    }
+}
+
+class Solution {
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        int m = s1.length(), n = s2.length();
+        int[][] d = new int[n][0];
+        for (int i = 0; i < n; ++i) {
+            int j = i;
+            int cnt = 0;
+            for (int k = 0; k < m; ++k) {
+                if (s1.charAt(k) == s2.charAt(j)) {
+                    if (++j == n) {
+                        j = 0;
+                        ++cnt;
+                    }
+                }
+            }
+            d[i] = new int[] { cnt, j };
+        }
+        int ans = 0;
+        for (int j = 0; n1 > 0; --n1) {
+            ans += d[j][0];
+            j = d[j][1];
+        }
+        return ans / n2;
+    }
+}
