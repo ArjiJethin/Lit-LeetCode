46981bd4f124367a769949422fc26e2c0c518cc0
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-09-09 19:27:51 +0530

    Add implementation for Problem483: Smallest Good Base

---
+++ Chapter 5/Problem483.java
@@ -0,0 +1,51 @@
+public class Problem483 {
+    public static void main(String[] args) {
+        // Example usage
+        String n = "13";
+        Solution solution = new Solution();
+        String result = solution.smallestGoodBase(n);
+        System.out.println("Smallest good base: " + result); // Expected output: "3"
+    }
+}
+
+class Solution {
+    public String smallestGoodBase(String n) {
+        long num = Long.parseLong(n);
+        for (int len = 63; len >= 2; --len) {
+            long radix = getRadix(len, num);
+            if (radix != -1) {
+                return String.valueOf(radix);
+            }
+        }
+        return String.valueOf(num - 1);
+    }
+
+    private long getRadix(int len, long num) {
+        long l = 2, r = num - 1;
+        while (l < r) {
+            long mid = l + r >>> 1;
+            if (calc(mid, len) >= num)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return calc(r, len) == num ? r : -1;
+    }
+
+    private long calc(long radix, int len) {
+        long p = 1;
+        long sum = 0;
+        for (int i = 0; i < len; ++i) {
+            if (Long.MAX_VALUE - sum < p) {
+                return Long.MAX_VALUE;
+            }
+            sum += p;
+            if (Long.MAX_VALUE / p < radix) {
+                p = Long.MAX_VALUE;
+            } else {
+                p *= radix;
+            }
+        }
+        return sum;
+    }
+}
