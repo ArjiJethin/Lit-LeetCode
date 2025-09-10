6701754b97f031f3f2d5cf9377257c51721f3d6f
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-09-10 18:01:00 +0530

    Add implementation for Problem496: Next Greater Element

---
+++ Chapter 5/Problem496.java
@@ -0,0 +1,37 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Problem496 {
+    public static void main(String[] args) {
+        // Example usage
+        int[] nums1 = { 4, 1, 2 };
+        int[] nums2 = { 1, 3, 4, 2 };
+        Solution solution = new Solution();
+        int[] result = solution.nextGreaterElement(nums1, nums2);
+        System.out.print("Next greater elements: ");
+        for (int num : result) {
+            System.out.print(num + " "); // Expected output: -1 3 -1
+        }
+    }
+}
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Deque<Integer> stk = new ArrayDeque<>();
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int num : nums2) {
+            while (!stk.isEmpty() && stk.peek() < num) {
+                mp.put(stk.pop(), num);
+            }
+            stk.push(num);
+        }
+        int n = nums1.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i) {
+            ans[i] = mp.getOrDefault(nums1[i], -1);
+        }
+        return ans;
+    }
+}
