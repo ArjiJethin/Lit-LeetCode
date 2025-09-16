8713f6d6b11875f76d60c7f3a1572c198e6154f6
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-09-16 15:26:53 +0530

    Add implementation for Problem155: Initial class file with solution for MinStack

---
+++ Page 2/Problem155.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Problem155 {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // Returns -3.
+        minStack.pop();
+        System.out.println(minStack.top()); // Returns 0.
+        System.out.println(minStack.getMin()); // Returns -2.
+    }
+}
+
+class MinStack {
+    private Deque<Integer> stk1 = new ArrayDeque<>();
+    private Deque<Integer> stk2 = new ArrayDeque<>();
+
+    public MinStack() {
+        stk2.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stk1.push(val);
+        stk2.push(Math.min(val, stk2.peek()));
+    }
+
+    public void pop() {
+        stk1.pop();
+        stk2.pop();
+    }
+
+    public int top() {
+        return stk1.peek();
+    }
+
+    public int getMin() {
+        return stk2.peek();
+    }
+}
