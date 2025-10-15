51ccb7708884ee737cb7836183c3aa5484693d56
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-10-15 19:44:19 +0530

    Add implementation for Problem513: Initial class file with solution for finding the bottom left value in a binary tree

---
+++ Page 6/Problem513.java
@@ -0,0 +1,23 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int ans = 0;
+        while (!q.isEmpty()) {
+            ans = q.peek().val;
+            for (int i = q.size(); i > 0; --i) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+        return ans;
+    }
+}
