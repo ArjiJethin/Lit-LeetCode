6220b960f615e9cdf56db90041a9cd82e2bd5e6e
Eswar Arji <arjijethin.ac0705@gmail.com>
2025-11-08 22:09:44 +0530

    Add implementation of a C program demonstrating process creation using fork

---
+++ Miscellaneous/Practises/Practise 1/Program2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void main()
+{
+    int pid;
+    pid = fork();
+    if (pid == 0)
+    {
+        printf("Child Process...\n");
+        printf("Child Process ID: %d\n", getpid());
+        printf("Parent Process ID;%d\n", getppid());
+        printf("Finished with child\n");
+    }
+
+    else
+    {
+        wait(NULL);
+        printf("Parent Process...\n");
+        printf("Parent Process ID: %d\n", getpid());
+        printf("Child Process ID: %d\n", pid);
+    }
+}
