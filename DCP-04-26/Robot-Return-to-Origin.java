26275cb09b52075108b5cb123a743bf1aaad307e
Eswar Arji <arjijethin.ac0705@gmail.com>
2026-04-05 12:19:08 +0530

    [657] [Time Beats: 99.05%] [Memory Beats: 48.28%] - LeetPush

---
+++ DCP-04-26/Robot-Return-to-Origin.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public boolean judgeCircle(String moves) {
+3        int x = 0, y = 0;
+4        for (char move: moves.toCharArray()) {
+5            if (move == 'U') y--;
+6            else if (move == 'D') y++;
+7            else if (move == 'L') x--;
+8            else if (move == 'R') x++;
+9        }
+10        return x == 0 && y == 0;
+11    }
+12}
