d877c65e34720539b5daabcf82aef6d399e35d18
Eswar Arji <arjijethin.ac0705@gmail.com>
2026-04-18 13:52:21 +0530

    [3783] [Time Beats: 100.00%] [Memory Beats: 65.56%] - LeetPush

---
+++ DCP-04-26/Mirror-Distance-of-an-Integer.java
@@ -0,0 +1,15 @@
+1class Solution {
+2
+3    public int reverse(int n) {
+4        int res = 0;
+5        while (n > 0) {
+6            res = res * 10 + (n % 10);
+7            n /= 10;
+8        }
+9        return res;
+10    }
+11
+12    public int mirrorDistance(int n) {
+13        return Math.abs(n - reverse(n));
+14    }
+15}
