e50083fe197b281a130f3e791ac9980d6bd2b95d
Eswar Arji <arjijethin.ac0705@gmail.com>
2026-04-19 15:09:02 +0530

    [1855] [Time Beats: 100.00%] [Memory Beats: 98.31%] - LeetPush

---
+++ DCP-04-26/Maximum-Distance-Between-a-Pair-of-Values.java
@@ -0,0 +1,11 @@
+1class Solution {
+2    public int maxDistance(int[] A, int[] B) {
+3        int i, j;
+4
+5        for (i = 0, j = 0; i < A.length && j < B.length; j++)
+6            if (A[i] > B[j])
+7                i++;
+8
+9        return Math.max(0, j - i - 1);
+10    }
+11}
