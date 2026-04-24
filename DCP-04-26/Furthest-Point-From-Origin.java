e549d2aaa051c0849cdda15ce79afaf4b2c4e9ee
Eswar Arji <arjijethin.ac0705@gmail.com>
2026-04-24 17:57:59 +0530

    [2833] [Time Beats: 100.00%] [Memory Beats: 64.29%] - LeetPush

---
+++ DCP-04-26/Furthest-Point-From-Origin.java
@@ -0,0 +1,18 @@
+1class Solution {
+2
+3    public int furthestDistanceFromOrigin(String moves) {
+4        int L = 0;
+5        int R = 0;
+6        int B = 0;
+7        for (char c : moves.toCharArray()) {
+8            if (c == 'L') {
+9                L++;
+10            } else if (c == 'R') {
+11                R++;
+12            } else {
+13                B++;
+14            }
+15        }
+16        return Math.abs(L - R) + B;
+17    }
+18}
